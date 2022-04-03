278096efb933b2c31eca162b29e082d14b35dd30
Yosbel Marin <yosbel@x.com>
2022-04-02 22:23:38 -0300

    replace cli app

--- bin/remote-func.ts
+++ bin/remote-func.ts
@@ -1,13 +1,26 @@
 #!/usr/bin/env node
 import { extractDts } from '../dev-tools/extract-dts'
 import fs from 'fs'
-const mri = require('mri')
-const opts = mri(process.argv.slice(2))
+import sade from 'sade'
+const pkg = require('../package.json')
 
-if (opts['extract-dts']) {
-  const { source, out } = opts
-  if (!source) throw new Error(`--source is mandatory`)
-  if (!out) throw new Error(`--out is mandatory`)
-  if (!fs.existsSync(source)) throw new Error(`${source} does not exists`)
-  extractDts(source, out)
-}
+const prog = sade('remote-func')
+
+prog
+  .version(pkg.version)
+
+prog
+  .command('extract-dts')
+  .describe('Extract .dts files and .js stub')
+  .option('-s, --source', 'Source file name')
+  .option('-o, --out', 'Output directory')
+  .example('export-dts --source my-api.js --out ./dts-api-dir')
+  .action((opts) => {
+    const { source, out } = opts
+    if (!source) throw new Error(`--source is mandatory`)
+    if (!out) throw new Error(`--out is mandatory`)
+    if (!fs.existsSync(source)) throw new Error(`${source} does not exists`)
+    extractDts(source, out)
+  })
+
+prog.parse(process.argv)
